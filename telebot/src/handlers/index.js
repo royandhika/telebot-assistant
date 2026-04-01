615e2fcee7dd4055dd71db2db76c1d0e593ba430
royandhika <satriaroy70@gmail.com>
2026-04-01 21:39:58 +0700

    refactor: remove commented-out middleware for performance optimization

--- telebot/src/handlers/index.js
+++ telebot/src/handlers/index.js
@@ -2,13 +2,6 @@ import logger from '../logger/logger.js';
 import { handleTextMessage } from './textHandler.js';
 
 function setupHandlers(bot) {
-  // bot.use(async (ctx, next) => {
-  //   const start = Date.now();
-  //   await next();
-  //   const ms = Date.now() - start;
-  //   logger.debug(`Update processed in ${ms}ms`);
-  // });
-
   bot.start((ctx) => {
     logger.info(`User ${ctx.from.id} (${ctx.from.username}) started the bot.`);
     ctx.reply('Welcome to ETL Assistant Bot!')
